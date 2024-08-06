879200a0a26acd67485c322804edfb8175a9b9c2
Reno Mardiputra <57384764+RenoTxT@users.noreply.github.com>
2024-08-06 13:20:56 +0700

    Add files via upload

---
+++ calculator/calculator.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace calculator
+{
+    partial class calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.negate = new System.Windows.Forms.Button();
+            this.zero0 = new System.Windows.Forms.Button();
+            this.pointKoma = new System.Windows.Forms.Button();
+            this.Summing = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.three3 = new System.Windows.Forms.Button();
+            this.two2 = new System.Windows.Forms.Button();
+            this.one1 = new System.Windows.Forms.Button();
+            this.multiply = new System.Windows.Forms.Button();
+            this.nine9 = new System.Windows.Forms.Button();
+            this.eight8 = new System.Windows.Forms.Button();
+            this.seven7 = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.six6 = new System.Windows.Forms.Button();
+            this.five5 = new System.Windows.Forms.Button();
+            this.four4 = new System.Windows.Forms.Button();
+            this.deleteAll = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.divide = new System.Windows.Forms.Button();
+            this.angka_history = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // negate
+            // 
+            this.negate.Location = new System.Drawing.Point(12, 293);
+            this.negate.Name = "negate";
+            this.negate.Size = new System.Drawing.Size(50, 50);
+            this.negate.TabIndex = 0;
+            this.negate.Text = "+/-";
+            this.negate.UseVisualStyleBackColor = true;
+            // 
+            // zero0
+            // 
+            this.zero0.Location = new System.Drawing.Point(68, 293);
+            this.zero0.Name = "zero0";
+            this.zero0.Size = new System.Drawing.Size(50, 50);
+            this.zero0.TabIndex = 1;
+            this.zero0.Text = "0";
+            this.zero0.UseVisualStyleBackColor = true;
+            this.zero0.Click += new System.EventHandler(this.zero0_Click);
+            // 
+            // pointKoma
+            // 
+            this.pointKoma.Location = new System.Drawing.Point(124, 293);
+            this.pointKoma.Name = "pointKoma";
+            this.pointKoma.Size = new System.Drawing.Size(50, 50);
+            this.pointKoma.TabIndex = 2;
+            this.pointKoma.Text = ".";
+            this.pointKoma.UseVisualStyleBackColor = true;
+            this.pointKoma.Click += new System.EventHandler(this.pointKoma_Click);
+            // 
+            // Summing
+            // 
+            this.Summing.Location = new System.Drawing.Point(124, 69);
+            this.Summing.Name = "Summing";
+            this.Summing.Size = new System.Drawing.Size(106, 50);
+            this.Summing.TabIndex = 3;
+            this.Summing.Text = "=";
+            this.Summing.UseVisualStyleBackColor = true;
+            this.Summing.Click += new System.EventHandler(this.Summing_Click);
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(181, 293);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(50, 50);
+            this.plus.TabIndex = 7;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // three3
+            // 
+            this.three3.Location = new System.Drawing.Point(124, 237);
+            this.three3.Name = "three3";
+            this.three3.Size = new System.Drawing.Size(50, 50);
+            this.three3.TabIndex = 6;
+            this.three3.Text = "3";
+            this.three3.UseVisualStyleBackColor = true;
+            // 
+            // two2
+            // 
+            this.two2.Location = new System.Drawing.Point(68, 237);
+            this.two2.Name = "two2";
+            this.two2.Size = new System.Drawing.Size(50, 50);
+            this.two2.TabIndex = 5;
+            this.two2.Text = "2";
+            this.two2.UseVisualStyleBackColor = true;
+            // 
+            // one1
+            // 
+            this.one1.Location = new System.Drawing.Point(12, 237);
+            this.one1.Name = "one1";
+            this.one1.Size = new System.Drawing.Size(50, 50);
+            this.one1.TabIndex = 4;
+            this.one1.Text = "1";
+            this.one1.UseVisualStyleBackColor = true;
+            // 
+            // multiply
+            // 
+            this.multiply.Location = new System.Drawing.Point(180, 181);
+            this.multiply.Name = "multiply";
+            this.multiply.Size = new System.Drawing.Size(50, 50);
+            this.multiply.TabIndex = 15;
+            this.multiply.Text = "x";
+            this.multiply.UseVisualStyleBackColor = true;
+            this.multiply.Click += new System.EventHandler(this.multiply_Click);
+            // 
+            // nine9
+            // 
+            this.nine9.Location = new System.Drawing.Point(124, 125);
+            this.nine9.Name = "nine9";
+            this.nine9.Size = new System.Drawing.Size(50, 50);
+            this.nine9.TabIndex = 14;
+            this.nine9.Text = "9";
+            this.nine9.UseVisualStyleBackColor = true;
+            // 
+            // eight8
+            // 
+            this.eight8.Location = new System.Drawing.Point(68, 125);
+            this.eight8.Name = "eight8";
+            this.eight8.Size = new System.Drawing.Size(50, 50);
+            this.eight8.TabIndex = 13;
+            this.eight8.Text = "8";
+            this.eight8.UseVisualStyleBackColor = true;
+            // 
+            // seven7
+            // 
+            this.seven7.Location = new System.Drawing.Point(12, 125);
+            this.seven7.Name = "seven7";
+            this.seven7.Size = new System.Drawing.Size(50, 50);
+            this.seven7.TabIndex = 12;
+            this.seven7.Text = "7";
+            this.seven7.UseVisualStyleBackColor = true;
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(180, 237);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(50, 50);
+            this.minus.TabIndex = 11;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // six6
+            // 
+            this.six6.Location = new System.Drawing.Point(124, 181);
+            this.six6.Name = "six6";
+            this.six6.Size = new System.Drawing.Size(50, 50);
+            this.six6.TabIndex = 10;
+            this.six6.Text = "6";
+            this.six6.UseVisualStyleBackColor = true;
+            // 
+            // five5
+            // 
+            this.five5.Location = new System.Drawing.Point(68, 181);
+            this.five5.Name = "five5";
+            this.five5.Size = new System.Drawing.Size(50, 50);
+            this.five5.TabIndex = 9;
+            this.five5.Text = "5";
+            this.five5.UseVisualStyleBackColor = true;
+            // 
+            // four4
+            // 
+            this.four4.Location = new System.Drawing.Point(12, 181);
+            this.four4.Name = "four4";
+            this.four4.Size = new System.Drawing.Size(50, 50);
+            this.four4.TabIndex = 8;
+            this.four4.Text = "4";
+            this.four4.UseVisualStyleBackColor = true;
+            // 
+            // deleteAll
+            // 
+            this.deleteAll.Location = new System.Drawing.Point(68, 69);
+            this.deleteAll.Name = "deleteAll";
+            this.deleteAll.Size = new System.Drawing.Size(50, 50);
+            this.deleteAll.TabIndex = 18;
+            this.deleteAll.Text = "C";
+            this.deleteAll.UseVisualStyleBackColor = true;
+            this.deleteAll.Click += new System.EventHandler(this.deleteAll_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(13, 69);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(49, 50);
+            this.delete.TabIndex = 17;
+            this.delete.Text = "CE";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(218, 29);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // divide
+            // 
+            this.divide.Location = new System.Drawing.Point(181, 125);
+            this.divide.Name = "divide";
+            this.divide.Size = new System.Drawing.Size(50, 50);
+            this.divide.TabIndex = 21;
+            this.divide.Text = "/";
+            this.divide.UseVisualStyleBackColor = true;
+            this.divide.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // angka_history
+            // 
+            this.angka_history.Location = new System.Drawing.Point(131, 8);
+            this.angka_history.Name = "angka_history";
+            this.angka_history.Size = new System.Drawing.Size(100, 20);
+            this.angka_history.TabIndex = 22;
+            this.angka_history.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 358);
+            this.Controls.Add(this.angka_history);
+            this.Controls.Add(this.divide);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.deleteAll);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.multiply);
+            this.Controls.Add(this.nine9);
+            this.Controls.Add(this.eight8);
+            this.Controls.Add(this.seven7);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.six6);
+            this.Controls.Add(this.five5);
+            this.Controls.Add(this.four4);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.three3);
+            this.Controls.Add(this.two2);
+            this.Controls.Add(this.one1);
+            this.Controls.Add(this.Summing);
+            this.Controls.Add(this.pointKoma);
+            this.Controls.Add(this.zero0);
+            this.Controls.Add(this.negate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "calculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button negate;
+        private System.Windows.Forms.Button zero0;
+        private System.Windows.Forms.Button pointKoma;
+        private System.Windows.Forms.Button Summing;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button three3;
+        private System.Windows.Forms.Button two2;
+        private System.Windows.Forms.Button one1;
+        private System.Windows.Forms.Button multiply;
+        private System.Windows.Forms.Button nine9;
+        private System.Windows.Forms.Button eight8;
+        private System.Windows.Forms.Button seven7;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button six6;
+        private System.Windows.Forms.Button five5;
+        private System.Windows.Forms.Button four4;
+        private System.Windows.Forms.Button deleteAll;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button divide;
+        private System.Windows.Forms.TextBox angka_history;
+    }
+}
+
---
+++ calculator/calculator.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//program start from here
+//Created by Reno Mardiputra
+//Time: 05/08/2024
+//Donation via BRI: 0512 0102 3520 507
+
+namespace calculator
+{
+    public partial class calculator : Form
+    {
+        //declaration public variable and flex
+        private double a, b,c;
+        private string operation = "";
+        private bool toB = false;
+        private bool clickedOperation = false;
+        private bool isResult = false;
+        private bool multiplication = false;
+        private bool summingnonumber = false;
+
+
+        public calculator()
+        {
+            //intialize the component and the methods also setting all the view at 0 for the first time
+            InitializeComponent();
+            InitializeEventHandlers();
+            textBox1.Text = "0";
+            angka_history.Text = "0";
+        }
+
+
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            //this program is used for change the number at the text box by using flex, we can add more leght and separate the "a" variable and "b" variable
+            Button button = (Button)sender;
+            string number = button.Text;
+            if (summingnonumber == true)
+            {
+                textBox1.Text = number;
+                summingnonumber = false;
+            }
+            else
+            {
+                if (textBox1.Text == "0")
+                {
+                    // Replace "0" with the clicked number for "a" variable
+                    textBox1.Text = number;
+                }
+                else
+                {
+                    //when it's "b" variable it stuck at the last number and change to then number which you clicked
+                    if (clickedOperation == true || multiplication == true && toB == true)
+                    {
+                        textBox1.Text = number;
+                        clickedOperation = false;
+                    }
+                    else
+                    {
+                        // Append the clicked number to the existing text
+                        textBox1.Text += number;
+                    }
+                }
+            }
+        }
+
+        private void InitializeEventHandlers()
+        {
+             // Numeric buttons
+             this.one1.Click += new System.EventHandler(this.NumberButton_Click);
+             this.two2.Click += new System.EventHandler(this.NumberButton_Click);
+             this.three3.Click += new System.EventHandler(this.NumberButton_Click);
+             this.four4.Click += new System.EventHandler(this.NumberButton_Click);
+             this.five5.Click += new System.EventHandler(this.NumberButton_Click);
+             this.six6.Click += new System.EventHandler(this.NumberButton_Click);
+             this.seven7.Click += new System.EventHandler(this.NumberButton_Click);
+             this.eight8.Click += new System.EventHandler(this.NumberButton_Click);
+             this.nine9.Click += new System.EventHandler(this.NumberButton_Click);
+             this.zero0.Click += new System.EventHandler(this.NumberButton_Click);
+
+             // Methods buttons
+             this.deleteAll.Click += new System.EventHandler(this.deleteAll_Click);
+             this.pointKoma.Click += new System.EventHandler(this.pointKoma_Click);
+        }
+
+        private void deleteAll_Click(object sender, EventArgs e)
+        {
+            // It's for "C" at calculator when you pressed it, all the variable is gone and all the display set to 0
+            textBox1.Clear();
+            textBox1.Text = "0";
+            a = b = c = 0;
+            toB = false;
+            clickedOperation = false;
+            isResult = false;
+            multiplication = false;
+            summingnonumber = false;    
+            operation = "";
+            angka_history.Text = "0";
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            if (isResult)
+            {
+                // When a result is displayed, pressing "CE" should reset the text box for new input
+                textBox1.Text = "0";
+                isResult = false; // Reset the flag for new input
+            }
+            else
+            {
+                // When inputting a number, remove the last character
+                if (textBox1.Text.Length > 1)
+                {
+                    textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                }
+                else
+                {
+                    textBox1.Text = "0";
+                }
+            }
+        }
+
+        private void pointKoma_Click(object sender, EventArgs e)
+        {
+            // If there's no "." at the textbox, then you can add ".", else the program do nothing because there's no other code for that
+            if (!textBox1.Text.Contains("."))
+            {
+                textBox1.Text += ".";
+            }
+        }
+
+// problem is from here, need some maintenance
+        private void Summing_Click(object sender, EventArgs e)
+        {
+            if (toB)
+            {
+                b = double.Parse(textBox1.Text);
+            }
+            else
+            {
+                a = double.Parse(textBox1.Text);
+            }
+            switch (operation)
+            {
+                case "plus":
+                    c = a + b;
+                    angka_history.Text = a + " + " + b;
+                    multiplication = true;
+                    break;
+                case "minus":
+                    c = a - b;
+                    angka_history.Text = a + " - " + b;
+                    multiplication = true;
+                    break;
+                case "multiply":
+                    c = a * b;
+                    angka_history.Text = a + " × " + b;
+                    multiplication = true;
+                    break;
+                case "divide":
+                    if (textBox1.TabStop                                                                                                                                                                                                                                                                                            )
+                    {
+                        // Perform division
+                        c = a / b;
+                        angka_history.Text = a + " ÷ " + b;
+                        multiplication = true;
+                    }
+                    else
+                    {
+                        // Handle division by zero
+                        textBox1.Text = "Cannot Divide by 0";
+                        // Reset the state of the calculator
+                        a = 0;
+                        b = 0;
+                        c = 0;
+                        operation = "";
+                        toB = false;
+                        clickedOperation = false;
+                    }
+                    break;
+                default:
+                    c = double.Parse(textBox1.Text);
+                    angka_history.Text = c.ToString();
+                    summingnonumber = true;
+                    break;
+            }
+            textBox1.Text = c.ToString();
+            toB = false;
+            a = c;
+            c = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (toB)
+            {
+                b = 
+                c = a / b;
+                angka_history.Text = a + " ÷ " + b;
+                textBox1.Text = c.ToString();
+                a = c;
+                c = 0;
+                multiplication = true;
+            }
+            else
+            {
+                a = double.Parse(textBox1.Text);
+                angka_history.Text = textBox1.Text + " ÷ ";
+                operation = "divide";
+                toB = true;
+                clickedOperation = true;
+            }
+        }
+
+        private void multiply_Click(object sender, EventArgs e)
+        {
+            if (toB)
+            {
+                b = double.Parse(textBox1.Text);
+                c = a * b;
+                angka_history.Text = a + " × " + b;
+                textBox1.Text = c.ToString();
+                a = c;
+                c = 0;
+                multiplication = true;
+            }
+            else
+            {
+                a = double.Parse(textBox1.Text);
+                angka_history.Text = textBox1.Text + " × ";
+                operation = "multiply";
+                toB = true;
+                clickedOperation = true;
+            }
+        }
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            if (toB)
+            {
+                b = double.Parse(textBox1.Text);
+                c = a + b;
+                angka_history.Text = a + " + " + b;
+                textBox1.Text = c.ToString();
+                a = c;
+                c = 0;
+                multiplication = true;
+            }
+            else
+            {
+                a = double.Parse(textBox1.Text);
+                angka_history.Text = textBox1.Text + " + ";
+                operation = "plus";
+                toB = true;
+                clickedOperation = true;
+            }
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            if (toB)
+            {
+               
+                b = double.Parse(textBox1.Text);
+                c = a - b;
+                angka_history.Text = a + " - " + b;
+                textBox1.Text = c.ToString();
+                a = c;
+                c = 0;
+                multiplication = true;
+            }
+            else
+            {
+                a = double.Parse(textBox1.Text);
+                angka_history.Text = textBox1.Text + " - ";
+                operation = "minus";
+                toB = true;
+                clickedOperation = true;
+
+            }
+        }
+
+        private void negate_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox1.Text, out double currentValue))
+            {
+                // Negate the value
+                currentValue = -currentValue;
+
+                // Update textBox1 with the negated value
+                textBox1.Text = currentValue.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void zero0_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
+
+
+//problems:
+//1. The divide by 0 still not showing
